d3b6df5b204920f6c40f57fca8d5fa0a6dbc440b
avallonem <avallonem@gmail.com>
2017-11-14 11:52:39 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+app.use(express.static("public"));
 // launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
